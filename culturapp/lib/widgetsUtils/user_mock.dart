ae38b38c36d9fb11c9588fe40b8a70787ec160b1
susssss23 <susannajaner@gmail.com>
2024-03-19 19:23:27 +0100

    mock up user

---
+++ culturapp/lib/widgetsUtils/user_mock.dart
@@ -0,0 +1,12 @@
+/*import 'package:firebase_auth/firebase_auth.dart';
+
+class UserMock extends User {
+  @override
+  String get uid => '2aR4ekwm4RUYdvPgteZLdSJUew04';
+
+  @override
+  String get email => 'hardcodeado@gmail.com';
+
+//@override
+//String getusername=> 'hardcodeado';
+}*/
