4e0256ae1ffca67a2d921e8d66f22565bd5275b9
AleixFIB <aleix.lopez.padro@estudiantat.upc.edu>
2024-03-22 08:50:12 +0100

    ara hauria de funcionar la part de front de mis actividades una vegada has fet login

---
+++ culturapp/lib/presentacio/screens/user_activities_future_builder.dart
@@ -0,0 +1,39 @@
+
+import 'package:culturapp/presentacio/controlador_presentacio.dart';
+import 'package:culturapp/presentacio/screens/vista_mis_actividades.dart';
+import 'package:flutter/material.dart';
+
+class UserActivitiesFutureBuilder extends StatelessWidget {
+  final String userId;
+  final ControladorPresentacion cp;
+
+  const UserActivitiesFutureBuilder({super.key, required this.userId, required this.cp});
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: cp.getUserActivities(userId),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        // While the future is not resolved
+        if (snapshot.connectionState != ConnectionState.done) {
+          // Return loading screen
+          return const CircularProgressIndicator();
+        }
+
+        // Once the future is resolved
+        if (snapshot.hasData) {
+          // Navigate to ListaMisActividades
+          return ListaMisActividades(
+            actividades: snapshot.data,
+          );
+        } else if (snapshot.hasError) {
+          // Handle error case
+          return Text('Error: ${snapshot.error}');
+        } else {
+          // Handle no data case
+          return const Text('No data');
+        }
+      },
+    );
+  }
+}
